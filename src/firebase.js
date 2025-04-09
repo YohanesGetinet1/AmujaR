44c77a97b19bd10fcc9c6f7df44e748021b99c7b
habtew <habtamuwolde92@gmail.com>
2025-04-09 21:55:00 +0530

    added .env file to secure api keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, push, get, remove, update } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAuwHxlHIHr2GdoJrGyR4x5dJRZlQkfOmw",
-    authDomain: "jodata-7bc36.firebaseapp.com",
-    databaseURL: "https://jodata-7bc36-default-rtdb.firebaseio.com/",
-    projectId: "jodata-7bc36",
-    storageBucket: "jodata-7bc36.appspot.com",
-    messagingSenderId: "397988027675",
-    appId: "1:397988027675:web:27e247d479852a0ad2c7be"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
